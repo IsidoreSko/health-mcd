9db88c153033b46650ee522afabe0d0fbee87c7a
IsidoreSKO <sparagmos@hotmail.fr>
2024-09-30 17:51:31 +0200

    login

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,14 @@
-"used client"
+"use client";
+
+import { useUserStore } from "@/lib/store/use-user-store";
+import LoginPage from "./login/page";
 
 export default function Home() {
-  return <div>Helllllo !!!!!</div>;
+  const userName = useUserStore((s) => s.userName);
+
+  if (!userName) {
+    return <LoginPage />;
+  }
+
+  return <div>Hello Healthdonals</div>;
 }
